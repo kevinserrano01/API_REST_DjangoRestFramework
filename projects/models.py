e2d4bb5aaa84fc6d15963049a352934f8cda261b
Kev <kevinserrano010@gmail.com>
2023-05-26 21:48:41 -0300

    Creación de modelo de proyecto con su respectiva migracion

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=200) # Titulo
+    description = models.TextField()
+    technology = models.CharField(max_length=200)# TEcnologia del proyecto
+    created_at = models.DateTimeField(auto_now_add=True) # Fecha de creacion del proyecto
+
+    def __str__(self):
+        return self.title
