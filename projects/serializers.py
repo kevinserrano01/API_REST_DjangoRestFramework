90964c2f8c4511e7365972836bf1b8027e5cb9f8
Kev <kevinserrano010@gmail.com>
2023-05-26 22:11:54 -0300

    incorporacion de ruta de app Projects en urls.py de simpleCRUD

--- projects/serializers.py
+++ projects/serializers.py
@@ -5,4 +5,4 @@ class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
         fields = ('id', 'title', 'description', 'technology', 'created_at') # Añade todos estos campos
-        read_only_fields = ('created_at') # Campos de solo lectura para no modificar ni elimnar la fecha de creacion.
+        read_only_fields = ('created_at',) # Campos de solo lectura para no modificar ni elimnar la fecha de creacion.
