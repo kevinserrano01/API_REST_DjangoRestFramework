c7bdae454addec93539e7658156428669458171b
Kev <kevinserrano010@gmail.com>
2023-05-27 11:18:38 -0300

    colocamos sqlite por defecto

--- simpleCRUD/settings.py
+++ simpleCRUD/settings.py
@@ -71,7 +71,7 @@ WSGI_APPLICATION = 'simpleCRUD.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        default='sqlite:///db.sqlite3',
         conn_max_age=600
     )
 }
