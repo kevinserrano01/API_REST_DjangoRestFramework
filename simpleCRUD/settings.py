b0c3c9ad471a4cb0b4769e46ec4ea049d1d3f826
Kev <kevinserrano010@gmail.com>
2023-05-27 11:16:42 -0300

    Conexion a la Base de datos

--- simpleCRUD/settings.py
+++ simpleCRUD/settings.py
@@ -70,10 +70,10 @@ WSGI_APPLICATION = 'simpleCRUD.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        conn_max_age=600
+    )
 }
 
 
